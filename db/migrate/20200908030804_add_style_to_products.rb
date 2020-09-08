132e8f28659553083b5f9970b6d39bfc874f2c98
Yan P Aung <yan.pye.aung@gmail.com>
2020-09-07 20:22:32 -0700

    Update migration as separate up and down methods

--- db/migrate/20200908030804_add_style_to_products.rb
+++ db/migrate/20200908030804_add_style_to_products.rb
@@ -1,5 +1,9 @@
 class AddStyleToProducts < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_reference :products, :style, null: false, foreign_key: true
+  end
+
+  def down
+    remove_reference :products, :style
   end
 end
